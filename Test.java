545fc84ddcbf02c84a81b7ae9a61e061eeeb5d4b
Yusuf Bedri Bitiren <bedri.bitiren@ug.bilkent.edu.tr>
2024-02-23 20:27:58 +0300

    Added test file for (as the name suggests) testing methods. Testing for Tile is done succesfully

---
+++ Test.java
@@ -0,0 +1,66 @@
+public class Test {
+    public static void main(String[] args) {
+        
+
+        // test matchingTiles
+
+        Tile tile1 = new Tile(5);
+        Tile tile2 = new Tile(5);
+        Tile tile3 = new Tile(10);
+        
+        System.out.println();
+        System.out.println("test matchingTiles");
+
+        System.out.println("Expected: true");
+        System.out.println(tile1.matchingTiles(tile2));
+
+        System.out.println("Expected: false");
+        System.out.println(tile1.matchingTiles(tile3));
+
+        
+        //test tile compareTo
+
+        System.out.println();
+        System.out.println("test tile compareTo");
+
+        System.out.println("Expected: 0");
+        System.out.println(tile1.compareTo(tile2));
+
+        System.out.println("Expected: -1");
+        System.out.println(tile1.compareTo(tile3));
+
+        System.out.println("Expected: 1");
+        System.out.println(tile3.compareTo(tile1));
+
+        // test canFormChainWith
+
+        Tile tile4 = new Tile(6);
+
+        System.out.println();
+        System.out.println("test canFormChainWith");
+
+        System.out.println("Expected: true");
+        System.out.println(tile1.canFormChainWith(tile4));
+
+        System.out.println("Expected: false");
+        System.out.println(tile1.canFormChainWith(tile3));
+
+        //test tile toString
+
+        System.out.println();
+        System.out.println("test tile toString");
+
+        System.out.println("Expected: 5");
+        System.out.println(tile1);
+
+        System.out.println("Expected: 10");
+        System.out.println(tile3);
+
+        System.out.println();
+        System.out.println("End of test for Tile");
+        System.out.println();
+
+        
+
+    }
+}
